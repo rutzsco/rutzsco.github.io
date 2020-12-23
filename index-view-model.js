483bb6c606eee9d95ec48fb95f70955ef1fcbb48
Scott Rutz <scrutz@microsoft.com>
2020-12-23 08:42:53 -0600

    Update index-view-model.js

--- index-view-model.js
+++ index-view-model.js
@@ -1,7 +1,16 @@
 function AppViewModel() {
     this.firstName = "Bert";
     this.lastName = "Bertington";
-}
 
+    this.sessions = ko.observableArray([]);
+
+    this.getData();
+}
+this.getData = function () {
+
+    $.getJSON("https://conferenceapi.azurewebsites.net/?format=json", function(data) { 
+
+    })
+};
 // Activates knockout.js
 ko.applyBindings(new AppViewModel());
