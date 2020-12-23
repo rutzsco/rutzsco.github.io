25668550f1f1c80b3dede54c05847553b3bb153c
Scott Rutz <scrutz@microsoft.com>
2020-12-23 08:45:49 -0600

    wip

--- index-view-model.js
+++ index-view-model.js
@@ -5,12 +5,15 @@ function AppViewModel() {
     this.sessions = ko.observableArray([]);
 
     this.getData();
+
+
+    this.getData = function () {
+        $.getJSON("https://conferenceapi.azurewebsites.net/sessions?format=json", function(data) { 
+    
+            this.sessions(data.items)
+        })
+    };
 }
-this.getData = function () {
 
-    $.getJSON("https://conferenceapi.azurewebsites.net/?format=json", function(data) { 
-
-    })
-};
 // Activates knockout.js
 ko.applyBindings(new AppViewModel());
